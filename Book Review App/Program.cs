def1c89225fa07af00e04f5470da6e4b92822c95
Radu Raul <raduraul198@gmail.com>
2024-11-01 15:36:35 +0200

    Comment to verify if works github

--- Book Review App/Program.cs
+++ Book Review App/Program.cs
@@ -16,6 +16,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Test project
+
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
