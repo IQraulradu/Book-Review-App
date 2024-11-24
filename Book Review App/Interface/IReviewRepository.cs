0e7131c8708fc86aa8e1cf63ce5bbfade2629dd2
Radu Raul <raduraul198@gmail.com>
2024-11-24 15:48:34 +0200

    Implementation of new methods for API DELETE request - IReview Repository - Review Repository - Review Controller

--- Book Review App/Interface/IReviewRepository.cs
+++ Book Review App/Interface/IReviewRepository.cs
@@ -10,6 +10,7 @@ namespace Book_Review_App.Interface
         bool ReviewExists(int reviewId);
         bool CreateReview(Review review);
         bool UpdateReview(Review review);
+        bool DeleteReview(Review review);
         bool Save();
 
     }
